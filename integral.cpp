5f71868644d865ab8de02102018b3a0562b461aa
IvanciniGT <ivan.osuna.ayuste@gmail.com>
2020-05-12 21:58:47 +0200

    Update integral.cpp

--- integral.cpp
+++ integral.cpp
@@ -8,34 +8,34 @@ double areaTrapecio(double altura1,double altura2,double a, double b){
 }
 
 double f(double x){
-    return x*x+2;
+    return x*x*x-x*x+2;
 }
 
 double integral(/*funcion,*/double a, double b, double tolerancia){
-    
+
     double ha, hb, m, hm;
     ha=f(a);
     hb=f(b);
     m=(a+b)/2;
     hm=f(m);
-    
+
     double areaT1, areaT2, areaTGrande;
     areaT1=areaTrapecio(ha,hm,a,m);
     areaT2=areaTrapecio(hm,hb,m,b);
     areaTGrande=areaTrapecio(ha,hb,a,b);
-    
+
     double error = std::abs(areaTGrande-areaT1-areaT2);
-    
+
     if(error>tolerancia){
         areaT1=integral(/*funcion,*/a, m, tolerancia);
         areaT2=integral(/*funcion,*/m, b, tolerancia);
     }
     return areaT1+areaT2;
-    
+
 }
 
 int main()
 {
-
+    std::cout << integral(-2,2,0.000000001);
     return 0;
 }
