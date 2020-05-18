e4260f28b85b870fff1fe7a1b321d888611cd9e5
IvanciniGT <ivan.osuna.ayuste@gmail.com>
2020-05-18 21:37:31 +0200

    Update integral.cpp

--- integral.cpp
+++ integral.cpp
@@ -11,11 +11,8 @@ double f(double x){
     return x*x*x-x*x+2;
 }
 
-double integral(/*funcion,*/double a, double b, double tolerancia, int &numeroDivisiones){
-    std::cout << ".";
+double integral(/*funcion,*/double a, double b, double tolerancia, int &numeroDivisiones, int maximoDivisionesPermitido){
 
-    if(++numeroDivisiones%80==0)
-        std::cout << std::endl;
 
     double ha, hb, m, hm;
     ha=f(a);
@@ -30,9 +27,15 @@ double integral(/*funcion,*/double a, double b, double tolerancia, int &numeroDi
 
     double error = std::abs(areaTGrande-areaT1-areaT2);
 
-    if(error>tolerancia){
-        areaT1=integral(/*funcion,*/a, m, tolerancia,numeroDivisiones);
-        areaT2=integral(/*funcion,*/m, b, tolerancia,numeroDivisiones);
+    if(error>tolerancia && numeroDivisiones<maximoDivisionesPermitido){
+        // Dividir mas
+        ++numeroDivisiones;
+        std::cout << ".";
+        if(numeroDivisiones%80==0)
+            std::cout << std::endl;
+        
+        areaT1=integral(/*funcion,*/a, m, tolerancia,numeroDivisiones, maximoDivisionesPermitido);
+        areaT2=integral(/*funcion,*/m, b, tolerancia,numeroDivisiones, maximoDivisionesPermitido);
     }
     return areaT1+areaT2;
 
@@ -41,7 +44,22 @@ double integral(/*funcion,*/double a, double b, double tolerancia, int &numeroDi
 int main()
 {
     int numeroInicialDivisiones=0;
-    double resultado=integral(-2,2,0.000000001,numeroInicialDivisiones);
+    double a, b ,tolerancia;
+    int maximoDivisionesPermitido;
+    // a
+    std::cout << "Dame el límite inferior de cálculo: ";
+    std::cin  >> a;
+    // b
+    std::cout << "Dame el límite superior de cálculo: ";
+    std::cin  >> b;
+    // tolerancia
+    std::cout << "Damela tolerancia: ";
+    std::cin  >> tolerancia;
+    // Nº max divisiones
+    std::cout << "Dame el máximo de divisiones posibles: ";
+    std::cin  >> maximoDivisionesPermitido;
+
+    double resultado=integral(-2,2,0.000000001,numeroInicialDivisiones, maximoDivisionesPermitido);
     std::cout <<std::endl << "La integral vale: "<< resultado;
     std::cout << std::endl << "He necesitado: "<<numeroInicialDivisiones<<" divisiones";
     return 0;
